8717ef4e4c8ecbcb8753f83c3b98752086a48a13
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 12:46:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Example
+{
+    static void Main()
+    {
+        string[] chockii = { "dairymilk", "luvit", "kitkat", "candy" };
+
+        
+        chockii[1] = "center fresh";
+
+        
+        foreach(string chocolate in chockii)
+        {
+            Console.WriteLine(chocolate);
+        }
+    }
+}
